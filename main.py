9ae1e6ea68477615f19617a91a86ecdb29de3cf6
sweetysweat <aursegov000@gmail.com>
2021-11-02 22:31:54 +0300

    Подключение библиотеки requests и проверка Интернет соединения

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+import requests
+import subprocess
+
+"""Here you can see all signs of VM"""
+VM_signs = {
+    "Internet Connection": "",
+}
+
+
+def check_internet_connection():
+    """Check Internet connection"""
+    try:
+        requests.get("https://www.google.com/", timeout=5)
+        VM_signs["Internet Connection"] = "Yes"
+    except requests.ConnectionError:
+        VM_signs["Internet Connection"] = "No"
+
+
 if __name__ == "__main__":
-    pass
+    print(check_internet_connection())
