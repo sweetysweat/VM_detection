e3c8c8c00dfe50ba1e537588632368b660e36f23
sweetysweat <aursegov000@gmail.com>
2021-11-03 01:51:29 +0300

    Проверка MAC

--- main.py
+++ main.py
@@ -1,20 +1,51 @@
-import requests
 import subprocess
+import os
+import requests
+
 
 """Here you can see all signs of VM"""
 VM_signs = {
-    "Internet Connection": "",
+    "Internet Connection:": "",
+    "MAC:": "",
 }
+count_signs = 0
+
+
+def execute_command(command: list[str]) -> list[str]:
+    """Run command in shell and save it for parsing"""
+    data = []
+    for line in subprocess.Popen(["Powershell.exe", *command], stdout=subprocess.PIPE).stdout:
+        if line.strip():
+            data.append(line.decode("cp866").strip())
+    return data
 
 
 def check_internet_connection():
     """Check Internet connection"""
+    global count_signs
     try:
         requests.get("https://www.google.com/", timeout=5)
-        VM_signs["Internet Connection"] = "Yes"
+        VM_signs["Internet Connection:"] = "Yes"
     except requests.ConnectionError:
-        VM_signs["Internet Connection"] = "No"
+        VM_signs["Internet Connection:"] = "No"
+        count_signs += 1
+
+
+def get_MAC():
+    """Run ipconfig in shell and find MAC"""
+    global count_signs
+    for row in execute_command(["ipconfig", "/all"]):
+        row = row.split()
+        if row[0] in ["Физический", "Physical"]:
+            if row[-1].startswith("00-0C-29"):
+                VM_signs["MAC:"] = f"{row[-1]} - standard VMware MAC"
+                count_signs += 1
+            else:
+                VM_signs["MAC:"] = "Real MAC"
+            break
 
 
 if __name__ == "__main__":
-    print(check_internet_connection())
+    check_internet_connection()
+    get_MAC()
+
