2a6a6abfe4eda8ea0de5d53513504158edcca76f
sweetysweat <aursegov000@gmail.com>
2021-11-03 16:41:00 +0300

    Распознавание модели машины

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
 import subprocess
-import os
+import re
 import requests
 
 
 """Here you can see all signs of VM"""
 VM_signs = {
-    "Internet Connection:": "",
-    "MAC:": "",
+    "Internet Connection": "",
+    "MAC": "",
+    "Machine model": ""
 }
 count_signs = 0
-
-
-def execute_command(command: list[str]) -> list[str]:
-    """Run command in shell and save it for parsing"""
-    data = []
-    for line in subprocess.Popen(["Powershell.exe", *command], stdout=subprocess.PIPE).stdout:
-        if line.strip():
-            data.append(line.decode("cp866").strip())
-    return data
+pattern = r"\b[Vv][Mm]ware\b|[Vv][Mm]"  # Pattern for detecting VM or VMware in a string
 
 
 def check_internet_connection():
@@ -25,27 +18,46 @@ def check_internet_connection():
     global count_signs
     try:
         requests.get("https://www.google.com/", timeout=5)
-        VM_signs["Internet Connection:"] = "Yes"
+        VM_signs["Internet Connection"] = "Yes"
     except requests.ConnectionError:
-        VM_signs["Internet Connection:"] = "No"
+        VM_signs["Internet Connection"] = "No"
         count_signs += 1
 
 
+def execute_command(command: list[str]) -> str:
+    """Run command in shell"""
+    return subprocess.Popen(["Powershell.exe", *command], stdout=subprocess.PIPE).stdout.read().decode("cp866")
+
+
 def get_MAC():
-    """Run ipconfig in shell and find MAC"""
+    """Runs ipconfig in shell and find MAC"""
     global count_signs
-    for row in execute_command(["ipconfig", "/all"]):
+    data = execute_command(["ipconfig", "/all"]).split()
+    for row in data:
         row = row.split()
         if row[0] in ["Физический", "Physical"]:
             if row[-1].startswith("00-0C-29"):
-                VM_signs["MAC:"] = f"{row[-1]} - standard VMware MAC"
+                VM_signs["MAC"] = f"{row[-1]} - standard VMware MAC"
                 count_signs += 1
             else:
-                VM_signs["MAC:"] = "Real MAC"
+                VM_signs["MAC"] = "Real MAC"
             break
+
+
+def get_Model():
+    """Runs get-wmiobject win32_computersystem | fl Model in shell to get model of machine"""
+    global count_signs
+    data = execute_command(["get-wmiobject", "win32_computersystem",
+                            "|", "fl", "model"]).strip().split()[-1]  # -1 to get only model
+    if re.search(pattern, data):
+        VM_signs["Machine model"] = data
+        count_signs += 1
+    else:
+        VM_signs["Machine model"] = "Unique model"
 
 
 if __name__ == "__main__":
     check_internet_connection()
     get_MAC()
-
+    get_Model()
+    print(VM_signs)
